90506126bc473c2d947ef7130d26aaf26441fdda
adityafawzan <fawzan89@gmail.com>
2019-11-05 16:22:45 +0700

    051119

--- master-pemasok.php
+++ master-pemasok.php
@@ -255,16 +255,7 @@ if(isset($_POST['tombol_hapus']))
 
 ?>
 <div class="content-wrapper">
-  <section class="content-header">
-    <h1>
-      <a style="color:black;" href="?id_nav_detail=<?php echo $id_nav_detail; ?>&refresh&daftar">Navigasi</a>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li><a href="navigasi.php?id_nav=<?php echo $id_nav; ?>"><?php echo $nav; ?></a></li>
-      <li><a href="?id_nav_detail=<?php echo $id_nav_detail; ?>&refresh&daftar">Pemasok</a></li>
-    </ol>
-  </section>
+  <?php include 'template/content_header.php';?>
   <?php
 //PAGE_DAFTAR
   if(isset($_GET['daftar']))
