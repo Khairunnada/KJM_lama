6186a210aa591bfc38ce8a19d9f2b080d0c49178
adityafawzan <fawzan89@gmail.com>
2019-11-05 16:04:58 +0700

    051119

--- config.php
+++ config.php
@@ -10,12 +10,12 @@ $tgl_sekarang = date('Y-m-d');
 $today = date_parse($tgl_sekarang);
 $bulan_now = $today['month'];
 $tahun_now = $today['year'];
-$db_isistem = mysqli_connect("localhost", "root", "", "isistem");
-$db_internal_system = mysqli_connect("localhost", "root", "", "internal_system");
-$db_accounting = mysqli_connect("localhost", "root", "", "accounting");
-$db_sistem_stok = mysqli_connect("localhost", "root", "", "sistem_stok");
-$db_kas = mysqli_connect("localhost", "root", "", "kas");
-$db_repository = mysqli_connect("localhost", "root", "", "repository");
+//$db_isistem = mysqli_connect("localhost", "root", "", "isistem");
+//$db_internal_system = mysqli_connect("localhost", "root", "", "internal_system");
+//$db_accounting = mysqli_connect("localhost", "root", "", "accounting");
+//$db_sistem_stok = mysqli_connect("localhost", "root", "", "sistem_stok");
+//$db_kas = mysqli_connect("localhost", "root", "", "kas");
+//$db_repository = mysqli_connect("localhost", "root", "", "repository");
 //$db_bahan = mysqli_connect("localhost", "root", "", "bahan");
 if(isset($_SESSION['id_user_'.$kode_perusahaan])) 
 {
